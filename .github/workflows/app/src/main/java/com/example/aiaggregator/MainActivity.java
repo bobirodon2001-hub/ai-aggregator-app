2fe259c0d02ee340cc5c1e82c96b86dc9d030a24
bobirodon2001-hub <bobirodon2001@gmail.com>
2026-08-15 12:23:44 +0800

    Add MainActivity for WebView implementation

---
+++ .github/workflows/app/src/main/java/com/example/aiaggregator/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.aiaggregator;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        webView = new WebView(this);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://10.0.2.2:3000"); // Адрес твоего локального сервера
+        
+        setContentView(webView);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
+
